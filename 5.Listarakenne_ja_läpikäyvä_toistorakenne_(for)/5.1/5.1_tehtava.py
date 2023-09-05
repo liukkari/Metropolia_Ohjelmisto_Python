73f28d016c1c3a3bc64d18ba2018e86ca2ebf62c
Liu <124497775+liukkari@users.noreply.github.com>
2023-09-05 10:40:09 +0300

    Update 5.1_tehtava.py

--- 5.Listarakenne_ja_läpikäyvä_toistorakenne_(for)/5.1/5.1_tehtava.py
+++ 5.Listarakenne_ja_läpikäyvä_toistorakenne_(for)/5.1/5.1_tehtava.py
@@ -5,7 +5,10 @@ lista = []
 arpakuutioiden_lukumäärä = int(input("Anna arpakuutioiden lukumäärä.\n"))
 print()
 
+summa = 0
 for i in range(arpakuutioiden_lukumäärä):
     arpaluku = random.randint(1, 6)
-    print(arpaluku)
+    summa += arpaluku
+    print(f"Tämän hetkinen noppien summa on {summa}")
+
 
