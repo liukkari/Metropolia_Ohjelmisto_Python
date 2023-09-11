2cf2c59c2cc2838e23f4f23b8f6b89735bf3d073
Liu <124497775+liukkari@users.noreply.github.com>
2023-09-11 13:34:58 +0300

    Update 6.5_tehtava.py

--- 6.Funktio/6.5/6.5_tehtava.py
+++ 6.Funktio/6.5/6.5_tehtava.py
@@ -1,10 +1,31 @@
-def ---(listaa):
-
-    return listaaa
+def filtteri(listaa):
+    uusi_lista = []
+    for luku in listaa:
+        if luku == 0:
+            continue
+        elif (luku % 2) != 0:
+            uusi_lista.append(luku)
+    return uusi_lista
 
 
 def main():
+
     lista = []
-    while
+    while True:
+        syote = input("Anna kokonaislukuja. Tyhjä lopettaa toiminnon.\n")
+        print()
+        if syote == "":
+            muutettu_lista = filtteri(lista)
+            break
+        else:
+            kokonaisluku = int(syote)
+            lista.append(kokonaisluku)
+
+    print("Alkuperäinen listasi on:")
+    print(lista)
+    print()
+    print("Editoitu listasi ilman parittomia lukuja on:")
+    print(muutettu_lista)
+
 
 main()
