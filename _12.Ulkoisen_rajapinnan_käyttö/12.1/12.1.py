fc8c63755257731080c7b9a8d1a9485da1c9bef8
liukkari <124497775+liukkari@users.noreply.github.com>
2023-10-29 19:47:40 +0200

    Tehtävä: 12.1 tehty

--- _12.Ulkoisen_rajapinnan_käyttö/12.1/12.1.py
+++ _12.Ulkoisen_rajapinnan_käyttö/12.1/12.1.py
@@ -1,5 +1,21 @@
+import requests
+
+
 # Tehtävä 12.1
 
 '''Kirjoita ohjelma, joka hakee ja tulostaa satunnaisen Chuck Norris -vitsin käyttäjälle.
     Käytä seuravalla sivulla esiteltävää rajapintaa: https://api.chucknorris.io/.
     Käyttäjälle on näytettävä pelkkä vitsin teksti.'''
+
+
+def main():
+
+    tehtava = "https://api.chucknorris.io/"
+    path = "jokes/random"
+    pyyntö = tehtava + path
+
+    print()
+    vastaus = requests.get(pyyntö).json()
+    print(vastaus['value'])
+
+main()
