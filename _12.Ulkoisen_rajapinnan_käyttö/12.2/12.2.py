138d60743ed95bbf0d75ef9552f1432ebd3b0a41
Liu <liulaulou@gmail.com>
2023-10-30 14:01:47 +0200

    Tehtävä: 12.2 tehty

--- _12.Ulkoisen_rajapinnan_käyttö/12.2/12.2.py
+++ _12.Ulkoisen_rajapinnan_käyttö/12.2/12.2.py
@@ -1,3 +1,5 @@
+import requests
+import json
 # Tehtävä 12.2
 
 '''Tutustu avoimeen OpenWeather-säärajapintaan: https://openweathermap.org/api.
@@ -6,3 +8,32 @@
 
     Palveluun rekisteröityminen on tarpeen, jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
     Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.'''
+
+def main():
+
+    haku = input("Minkä paikkakunnan säätilaa haetaan? (Esim. Helsinki)\n")
+    aste = input("Haluaisitko lämpötilan Celsius-asteina vai Kelvin-asteina? (C/K)\n").upper()
+    if aste == "C":
+        haettava_aste = "metric"
+
+    else:
+        haettava_aste = "imperial"
+
+    pyynto = f"https://api.openweathermap.org/data/2.5/weather?q={haku}" \
+             f"&appid=858ea62e6b3e3a6f2991edf5dc7282e0" \
+             f"&units={haettava_aste}"
+    vastaus = requests.get(pyynto).json()
+
+    print()
+
+    saatila = vastaus['weather'][0]['description']
+    lampotila = vastaus['main']['temp']
+
+    print(f"Paikkakunta: {haku}\nSäätila: {saatila}")
+    if aste == "C":
+        print(f"Lämpötila: {lampotila}ºC")
+    else:
+        print(f"Lämpötila: {lampotila}ºK")
+
+
+main()
