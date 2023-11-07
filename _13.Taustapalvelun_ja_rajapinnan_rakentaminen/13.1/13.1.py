3ca968e89ec5e541ac72c7ef5f52c569fe72ff20
liukkari <124497775+liukkari@users.noreply.github.com>
2023-11-07 20:14:27 +0200

    Tehtävä 13.1 tehty

--- _13.Taustapalvelun_ja_rajapinnan_rakentaminen/13.1/13.1.py
+++ _13.Taustapalvelun_ja_rajapinnan_rakentaminen/13.1/13.1.py
@@ -1,6 +1,37 @@
+import json
+from flask import Flask
+
 # Tehtävä 13.1
 
 '''Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei.
     Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
     Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
     Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.'''
+
+
+app = Flask(__name__)
+@app.route('/alkuluku/<luku>')
+def alkuluku(luku):
+    luku = int(luku)
+
+    vastaus = {
+        "number": luku,
+        "isPrime": True
+    }
+
+    if luku == 2:
+        vastaus = {
+            "number": luku,
+            "isPrime": True
+        }
+
+    else:
+        for numero in range(2, int(luku**0.5) + 1):
+            if luku % numero == 0:
+                vastaus= {"number": luku, "isPrime": False}
+
+    return json.dumps(vastaus)
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, port=3001)
